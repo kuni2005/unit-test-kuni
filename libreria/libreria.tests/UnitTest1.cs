98eaf01f85d67c8038628609dbf4ffd0cd9feb06
Mathias <math.kuni123@gmail.com>
2025-04-21 22:58:12 -0500

    feat: unit tests

---
+++ libreria/libreria.tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using libreria;
+
+namespace libreria.tests
+{
+    public class LibraryTests
+    {
+        [Test]
+        public void Should_AddBook_And_FindIt()
+        {
+            var library = new Library();
+            library.AddBook("Clean Code");
+
+            Assert.IsTrue(library.HasBook("Clean Code"));
+        }
+
+        [Test]
+        public void Should_ReturnFalse_IfBookNotExists()
+        {
+            var library = new Library();
+
+            Assert.IsFalse(library.HasBook("The Pragmatic Programmer"));
+        }
+
+        [Test]
+        public void Should_Allow_DuplicatedBooks()
+        {
+            var library = new Library();
+            library.AddBook("Domain-Driven Design");
+            library.AddBook("Domain-Driven Design");
+
+            Assert.IsTrue(library.HasBook("Domain-Driven Design"));
+        }
+    }
+}
---
+++ libreria/libreria/Library.cs
@@ -0,0 +1,17 @@
+﻿namespace libreria
+{
+    public class Library
+    {
+        private List<string> books = new();
+
+        public void AddBook(string bookTitle)
+        {
+            books.Add(bookTitle);
+        }
+
+        public bool HasBook(string bookTitle)
+        {
+            return books.Contains(bookTitle);
+        }
+    }
+}
